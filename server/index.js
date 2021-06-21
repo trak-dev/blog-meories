fd137df75b721585c6a5679c242fbbf50ce53ed6
trak-dev <yann.picaud@gmail.com>
2021-06-21 15:51:58 +0200

    z

--- server/index.js
+++ server/index.js
@@ -1,13 +1,32 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const dotenv = require("dotenv");
+import fs from "fs";
+import https from "https";
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
 
-const postRoutes = require("./routes/posts.js");
-const userRoutes = require("./routes/users.js");
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
 
 const app = express();
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/yann-picaud.com/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/yann-picaud.com/cert.pem",
+  "utf8"
+);
+const ca = fs.readFileSync(
+  "/etc/letsencrypt/live/yann-picaud.com/chain.pem",
+  "utf8"
+);
+const credentials = {
+  key: privateKey,
+  cert: certificate,
+  ca: ca,
+};
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -16,7 +35,7 @@ app.use(cors());
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 const CONNECTION_URL = process.env.MONGO;
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("APP IS RUNNING");
 });
@@ -24,9 +43,9 @@ app.get("/", (req, res) => {
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
-    app.listen(PORT, () =>
-      console.log(`Server Running on Port: http://localhost:${PORT}`)
-    )
+    httpsServer.listen(8443, () => {
+      console.log("HTTPS Server running on port 8443");
+    })
   )
   .catch((error) => console.log(`${error} did not connect`));
 
