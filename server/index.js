462080198e4272298f45810b5a73c818772221d5
trak-dev <yann.picaud@gmail.com>
2021-07-02 16:52:56 +0200

    retour 5000

--- server/index.js
+++ server/index.js
@@ -1,5 +1,3 @@
-import fs from "fs";
-import https from "https";
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
@@ -10,23 +8,7 @@ import postRoutes from "./routes/posts.js";
 import userRoutes from "./routes/users.js";
 
 const app = express();
-const privateKey = fs.readFileSync(
-  "/etc/letsencrypt/live/yann-picaud.com/privkey.pem",
-  "utf8"
-);
-const certificate = fs.readFileSync(
-  "/etc/letsencrypt/live/yann-picaud.com/cert.pem",
-  "utf8"
-);
-const ca = fs.readFileSync(
-  "/etc/letsencrypt/live/yann-picaud.com/chain.pem",
-  "utf8"
-);
-const credentials = {
-  key: privateKey,
-  cert: certificate,
-  ca: ca,
-};
+
 dotenv.config();
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -39,14 +21,17 @@ const PORT = process.env.PORT || 5000;
 app.get("/", (req, res) => {
   res.send("APP IS RUNNING");
 });
-const httpsServer = https.createServer(credentials, app);
+
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    httpsServer.listen(8443, () => {
-      console.log("HTTPS Server running on port 8443");
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(
+    app.listen(PORT, () => {
+      console.log(`Server running at port ${port}`);
     })
   )
-  .catch((error) => console.log(`${error} did not connect`));
+  .catch((error) => console.log(error));
 
 mongoose.set("useFindAndModify", false);
